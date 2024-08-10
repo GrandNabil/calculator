c78aaffe026495649f38657bab6b7ecabf2791a1
Grand Nabil <akambilawani@gmail.com>
2024-08-10 09:18:47 +0100

    ajout de la vue pour la présentation du résultat

--- calculator/calculatorapp/views.py
+++ calculator/calculatorapp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html')
 
+
+def result(request):
+    num1 = float(request.GET.get('number1'))
+    num2 = float(request.GET.get('number2'))
+
+    if request.GET.get('addition') == "":
+        ans = num1 + num2
+
+    elif request.GET.get('soustraction') == "":
+        ans = num1 - num2
+
+    elif request.GET.get('multiplication') == "":
+        ans = num1 * num2
+
+    else:
+        ans = num1 / num2
+
+    return render(request, 'result.html', {'ans': ans})
